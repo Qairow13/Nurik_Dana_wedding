6b3f2a37290564f79e9f39f559441b2c436bd5ea
Qairow13 <kaiyrly29@gmail.com>
2026-08-11 09:18:38 +0500

    Update venue.ts

--- lib/venue.ts
+++ lib/venue.ts
@@ -1,6 +1,6 @@
 // Той орналасатын мекенжай туралы деректер. Өзгерту үшін осы жерді ғана түзетіңіз.
 export const VENUE_NAME = 'Той үйінің атауы';
-export const VENUE_ADDRESS = 'Алматы, Әуезов ауданы, Ақсай-4 шағын ауданы, 18а, 2-қабат';
+export const VENUE_ADDRESS = '"Premier Hall" мейрамханасы';
 export const VENUE_TIME = '17:00';
-export const VENUE_2GIS_URL = 'https://go.2gis.com/1VVgN';
-export const VENUE_GOOGLE_MAPS_URL = 'https://maps.app.goo.gl/vgA7mHvtU6NNxWkFA';
+export const VENUE_2GIS_URL = 'https://go.2gis.com/YSscN';
+export const VENUE_GOOGLE_MAPS_URL = 'https://maps.app.goo.gl/wFiiZ4wc4jfeVFMf7';
